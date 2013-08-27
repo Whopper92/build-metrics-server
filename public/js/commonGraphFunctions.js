2f037adfc4810523e717b6edc34f6df1d0f3b5b1
Will Hopper <whopper@puppetlabs.pdx.edu>
2013-08-27 14:04:08 -0700

    Remove tooltip footer text for histograms and tables

--- public/js/commonGraphFunctions.js
+++ public/js/commonGraphFunctions.js
@@ -16,13 +16,12 @@ function getLabelString(data) {
 
 /* Display a dynamically positioned tooltip on any graph type*/
 function displayTooltip(tooltipID, contentTitle, contentFooter, xPosition, yPosition, key, count, units) {
-
-  console.log(key)
   if(key == '') {
     var label = ''
   } else {
     var label = key + ': '
   }
+
   d3.select(tooltipID)
     .style('left', xPosition + 'px')
     .style('top', yPosition + 'px')
@@ -39,8 +38,6 @@ function displayTooltip(tooltipID, contentTitle, contentFooter, xPosition, yPosi
   d3.select(tooltipID)
     .style('left', xPosition + 'px')
     .style('top', yPosition + 'px')
-    .select(contentFooter)
-    .text('Click for additional data');
 
     d3.select(tooltipID).classed('hidden', false);
 }
